2c208ea6afd25c647839811b315ab3b504c1a2be
Mattia82Mattia <mattia.mercuriali@gmail.com>
2023-11-30 19:53:17 +0100

    Creato il tetramino quadrato

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include "include/common_include.h"
 #include <iostream>
 #include <ncurses.h>
+#include <unistd.h>
 
 using namespace std;
 
 int main() {
-    cout << addition(1, 2) << endl;
-    initscr();                     /* Start curses mode 		  */
-    printw("Hello World!!!! !!!"); /* Print Hello World		  */
-    refresh();                     /* Print it on to the real screen */
-    getch();                       /* Wait for user input */
-    endwin();                      /* End curses mode		  */
+    initscr(); /* Start curses mode 		  */
+    for (int i = 0; i < 10; i = i + 1) {
+        clear();
+        mvprintw(i, 0, "[][]");     /* Print Char		  */
+        mvprintw(i + 1, 0, "[][]"); /* Print Char		  */
+        sleep(1);
+        refresh();
+    }
+    getch();
+    endwin(); /* End curses mode		  */
     return 0;
 }
