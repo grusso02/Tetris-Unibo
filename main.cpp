8b66c6ff05a3b87e86a1e195829af269d5a2a33d
Mattia82Mattia <mattia.mercuriali@gmail.com>
2023-11-30 19:01:28 +0100

    Aggiunta !!!!

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
+#include "include/common_include.h"
 #include <iostream>
 #include <ncurses.h>
-#include "include/common_include.h"
 
 using namespace std;
 
 int main() {
     cout << addition(1, 2) << endl;
-    initscr();                 /* Start curses mode 		  */
-    printw("Hello World !!!"); /* Print Hello World		  */
-    refresh();                 /* Print it on to the real screen */
-    getch();                   /* Wait for user input */
-    endwin();                  /* End curses mode		  */
+    initscr();                     /* Start curses mode 		  */
+    printw("Hello World!!!! !!!"); /* Print Hello World		  */
+    refresh();                     /* Print it on to the real screen */
+    getch();                       /* Wait for user input */
+    endwin();                      /* End curses mode		  */
     return 0;
 }
