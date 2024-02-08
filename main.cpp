741ad74aa81eccb91a3fc468ee6e285eb2bf3ea8
luca-mar-01 <lucamarangon2001@gmail.com>
2024-02-08 12:40:19 +0100

    cosa

--- main.cpp
+++ main.cpp
@@ -15,12 +15,68 @@ int main() {
     noecho();
     curs_set(0);
 
-    Game game(HEIGHT, WIDTH);
 
-    while (!game.isOver()) {
-        game.updateState();
-        game.redraw();
+    *WINDOW = win;
+
+    int y = 0;
+    int selected = -1;
+    char pressed = NULL;
+
+    std::string choices[3] = {"START","BEST SCOES","EXIT"};
+
+    while(selected != -1){
+
+        for(int i=0;i<2;i++){
+            if(i == y)
+                wattron(win,A_STANDOUT);
+            print(i+1,0,choices[i].c_str());
+            wattroff(win,A_STANDOUT);
+        }
+        wrefresh();
+        // cambio puntatore
+        pressed = wgetch(win);
+        switch(pressed){
+            case KEY_DOWN:
+                if(y-1 >= 0)
+                    y--;
+                break;
+            case KEY_UP:
+                if(y+1 >= 0)
+                    y++;
+            case KEY_ENTER:
+                selected = y;
+                break;
+        }
     }
+    return selected;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Game game(HEIGHT, WIDTH);
+    Menu m(0,0,10,100);
+    while(m.getInput()){
+    }
+        
+
+    /*while (!game.isOver()) {
+
+        //game.updateState();
+        //game.redraw();
+    }*/
     endwin();
     cout << "Hai perso" << endl;
     return 0;
